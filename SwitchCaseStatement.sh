358d0d3890e56a30d44101d1226797302a7dba94
himanshunete <himanshuneteh@gmail.com>
2020-12-12 08:14:17 +0530

    UC-4: Solving using Switch Case Statement

---
+++ SwitchCaseStatement.sh
@@ -0,0 +1,58 @@
+#!/bin/bash/ -x
+Totalemp=100
+for((Empno=0;Empno<=$Totalemp;Empno++))
+do
+   Attendance=$((RANDOM%2))
+   if [ $Attendance -eq 0 ]
+   then
+      echo Employee no. $Empno is present
+      Check=$((RANDOM%2))
+      if [ $Check -eq 0 ]
+      then
+         echo Full Time
+      elif [ $Check -eq 1 ]
+      then
+         echo Part Time
+      fi
+   else
+      echo Employee no. $Empno is absent
+
+      Check=$((RANDOM%2))
+      if [ $Check -eq 0 ]
+      then
+         echo Full Time
+      elif [ $Check -eq 1 ]
+      then
+         echo Part Time
+      fi
+   fi
+done
+
+
+read UC
+UC1()
+{
+   WagePerHour=20
+   echo $WagePerHour Rs
+   FullDayHour=8
+   echo $FullDayHour hours
+   DailyEmployeeWage=$(($WagePerHour*$FullDayHour))
+   echo $DailyEmployeeWage Rs
+}
+
+UC2()
+{
+   PartTimeHour=8
+   PartTimeEmployees=50
+   PartTimeWagePerHour=20
+   PartTimeWage=$(($PartTimeHour*$PartTimeWagePerHour))
+   echo $PartTimeWage Rs per Employee
+   echo $PartTimeHour hours
+}
+
+
+case $UC in
+1) UC1 ;;
+2) UC2 ;;
+esac
+
