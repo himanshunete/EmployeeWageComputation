02fb5a2af3c898a38ab5ee377c1512b11d75b59f
himanshunete <himanshuneteh@gmail.com>
2020-12-12 06:09:35 +0530

    UC-3: Part Time of Employees

---
+++ empPartTimeUC.sh
@@ -0,0 +1,9 @@
+#!/bin/bash/ -x
+PartTimeHour=8
+PartTimeEmployees=50
+PartTimeWagePerHour=20
+PartTimeWage=$(($PartTimeHour*$PartTimeWagePerHour))
+echo $PartTimeWage Rs per Employee
+echo $PartTimeHour hours
+
+
