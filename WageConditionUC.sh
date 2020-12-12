5f4e904801cdd3a920038a01b231f8d343bf5fa2
himanshunete <himanshuneteh@gmail.com>
2020-12-12 08:16:03 +0530

    UC-6: Till Condition reched, calculate wages for a month

---
+++ WageConditionUC.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -x
+WagePerHour=20
+TotalWagePerMonth=0
+for ((totalhours=1;totalhours<=100;totalhours++))
+do
+   TotalWagePerMonth=$(($TotalWagePerMonth + $WagePerHour))
+done
+echo $TotalWagePerMonth
