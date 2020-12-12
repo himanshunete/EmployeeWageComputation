fdbebf856fd7d728ffd65e2f75b889712665ca7b
himanshunete <himanshuneteh@gmail.com>
2020-12-12 08:02:17 +0530

    UC-1: Checking the Attendance of Employees

---
+++ empAttendance.sh
@@ -0,0 +1,13 @@
+#!/bin/bash/ -x
+
+Totalemp=100
+for((Empno=0;Empno<=$Totalemp;Empno++))
+do
+   Attendance=$((RANDOM%2))
+   if [ $Attendance -eq 0 ]
+   then
+      echo Employee no. $Empno is present
+   else
+      echo Employee no. $Empno is absent
+   fi
+done
